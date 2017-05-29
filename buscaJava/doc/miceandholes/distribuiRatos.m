d925cccf10a02d7badd029b99d20b461979b41d5
Cleber Jorge Amaral <cleber.amaral@ifsc.edu.br>
2017-05-29 20:27:03 -0300

    Algoritmos parecem estar rodando corretamente. Apenas o BPI está apresentando profundidade sempre 1 nível acima do máximo, teria que averiguar este problema e se isso impacta em seus resultados.

--- buscaJava/doc/miceandholes/distribuiRatos.m
+++ buscaJava/doc/miceandholes/distribuiRatos.m
@@ -1,15 +1,41 @@
 %Original
-%m = [4 0 6 7 19 4 8 9 17 10];
-%h = [0 1 6 7 0 0 0 0 9 1 0 0 2 1 0 0 2 5 0 1];
-%d = size(m)(2)/sum(h)
+m = [4, 0, 6, 7, 19, 4, 8, 9, 17, 10];
+h = [0 1 6 7 0 0 0 0 9 1 0 0 2 1 0 0 2 5 0 1];
+d = size(m)(2)/sum(h)
 
 %Cria 10x mais ratos e distribui em um corredor 2 x maior
-%m1 = [];
-%h1 = [];
-%for i = 1: (size(h)(2)*10) h1 = [dist1 h(randi(numel(h)))]; end;
-%for i = 1: (size(m)(2)*10) m1 = [m1 randi(size(h1))]; end;
-%d1 = size(m1)(2)/sum(h1)
+m1 = [];
+h1 = [];
+for i = 1: (size(h)(2)*30) h1 = [h1 h(randi(numel(h)))]; end;
+for i = 1: (size(m)(2)*30) m1 = [m1 randi(size(h1)(2))]; end;
+d1 = size(m1)(2)/sum(h1)
 
+
+%Normaliza se a diferença da taxa de ocupação for maior que 1%
+while ((d/d1 > 1.01) || (d1/d < 0.99))
+	p = randi(size(h1)(2));
+	if (d/d1 > 1) %deve-se diminuir vagas
+		if (h1(p) > 0)
+			h1(p) = h1(p) - 1;
+		end
+	else %deve-se aumentar vagas
+		if (h1(p) < size(h1)(2))
+			h1(p) = h1(p) + 1;
+		end;
+	end;
+	d1 = size(m1)(2)/sum(h1);
+end;
+size(m1)(2)
+
+fprintf('int micePosition[] = {')
+fprintf('%d,',m1)
+fprintf('};\n')
+fprintf('int holeCapacity[] = {')
+fprintf('%d,',h1)
+fprintf('};\n')
+
+
+%{
 m1 = [4, 0, 6, 7, 9, 4, 8, 1, 17, 19];
 h1 = [0, 1, 6, 7, 0, 0, 0, 0, 9, 1, 0, 0, 2, 1, 0, 0, 2, 5, 0, 1];
 d1 = size(m1)(2)/sum(h1)
@@ -31,4 +57,4 @@ d1 = size(m1)(2)/sum(h1)
 m1 = [4, 0, 6, 7];
 h1 = [0, 1, 6, 7, 0, 0, 0, 0];
 d1 = size(m1)(2)/sum(h1)
-
+%}
--- buscaJava/src/exemplos/MiceAndHoles.java
+++ buscaJava/src/exemplos/MiceAndHoles.java
@@ -16,68 +16,83 @@ import busca.Estado;
 import busca.Nodo;
 
 /**
- * @author Cleber Jorge Amaral 
+ * @author Cleber Jorge Amaral
  */
-public class MiceAndHoles implements Estado, Heuristica, Aleatorio{
+public class MiceAndHoles implements Estado, Heuristica, Aleatorio {
 
 	public String getDescricao() {
-		return
-				"One day Masha came home and noticed n mice in the corridor of her flat.\n"+ 
-				"Of course, she shouted loudly, so scared mice started to run to the holes\n"+
-				" in the corridor.\n"+
-				"The corridor can be represeted as a numeric axis with n mice and m holes on it.\n"+
-				"ith mouse is at the coordinate xi, and jth hole — at coordinate pj. jth hole\n"+
-				"has enough room for cj mice, so not more than cj mice can enter this hole.\n"+
-				"What is the minimum sum of distances that mice have to go through so that\n"+
-				" they all can hide in the holes? If ith mouse goes to the hole j, then its\n" +
-				"distance is |xi - pj|.";
+		return "One day Masha came home and noticed n mice in the corridor of her flat.\n"
+				+ "Of course, she shouted loudly, so scared mice started to run to the holes\n"
+				+ " in the corridor.\n"
+				+ "The corridor can be represeted as a numeric axis with n mice and m holes on it.\n"
+				+ "ith mouse is at the coordinate xi, and jth hole — at coordinate pj. jth hole\n"
+				+ "has enough room for cj mice, so not more than cj mice can enter this hole.\n"
+				+ "What is the minimum sum of distances that mice have to go through so that\n"
+				+ " they all can hide in the holes? If ith mouse goes to the hole j, then its\n"
+				+ "distance is |xi - pj|.";
 	}
 
 	/** atributos do estado */
-	List<Integer> origPosition = new ArrayList<Integer>(); //Usado para calcular o cuso
-	List<Integer> micePosition = new ArrayList<Integer>(); //Armazena a posição atual de cada rato
-	List<Integer> holeCapacity = new ArrayList<Integer>(); //A capacidade de cada buraco dadas as posições dos ratos
-	int ithMice = -1; //Comeca pelo Buraco -1 pois a geracao de sucessores incrementa
+	List<Integer> origPosition = new ArrayList<Integer>(); // Usado para
+															// calcular o cuso
+	List<Integer> micePosition = new ArrayList<Integer>(); // Armazena a posição
+															// atual de cada
+															// rato
+	List<Integer> holeCapacity = new ArrayList<Integer>(); // A capacidade de
+															// cada buraco dadas
+															// as posições dos
+															// ratos
+	int ithMice = -1; // Comeca pelo -1 (a geracao de sucessores incrementa)
+	boolean debug = true;
+
 	/** operacao que gerou o estado */
 
-	/** Cria cenario inicial*/
+	/** Cria cenario inicial */
 	public MiceAndHoles(int mP[], int hC[]) {
 		int maxMicePosition = 0;
-		
-		//Cria lista de ratos
-		System.out.print("micePosition(MiceAndHoles): ");
+
+		// Cria lista de ratos
+		if (debug)
+			System.out.print("micePosition(MiceAndHoles): ");
 		for (int i = 0; i < mP.length; i++) {
-			micePosition.add(mP[i]);
+			micePosition.add(0); // Começa todos em zero
 			origPosition.add(mP[i]);
-			if (mP[i] > maxMicePosition) maxMicePosition = mP[i];
-			System.out.print(micePosition.get(i)+" ");
+			if (mP[i] > maxMicePosition)
+				maxMicePosition = mP[i];
+			if (debug)
+				System.out.print(micePosition.get(i) + " ");
 		}
-		
-		//Cria lista de buracos, caso haja ratos em buracos inexistentes, cria buracos com capacidade zero
+
+		// Cria lista de buracos, caso haja ratos em buracos inexistentes, cria
+		// buracos com capacidade zero
 		for (int i = 0; i < hC.length; i++)
 			holeCapacity.add(hC[i]);
 		for (int i = hC.length; i <= maxMicePosition; i++)
 			holeCapacity.add(0);
-		
-		//Subtrai a capacidade onde há ratos
+
+		// Subtrai a capacidade onde há ratos
 		for (int i = 0; i < micePosition.size(); i++)
-			holeCapacity.set(micePosition.get(i), holeCapacity.get(micePosition.get(i))-1);
-		
-		//Imprime mapa de capacidade atualizado
-		System.out.print("holeCapacity(MiceAndHoles): ");
-		for (int i = 0; i < holeCapacity.size(); i++) 
-			System.out.print(holeCapacity.get(i)+" ");
-		System.out.print("\n");
+			holeCapacity.set(micePosition.get(i),
+					holeCapacity.get(micePosition.get(i)) - 1);
+
+		// Imprime mapa de capacidade atualizado
+		if (debug)
+			System.out.print("holeCapacity(MiceAndHoles): ");
+		for (int i = 0; i < holeCapacity.size(); i++)
+			if (debug)
+				System.out.print(holeCapacity.get(i) + " ");
+		if (debug)
+			System.out.print("\n");
 	}
 
-	public boolean ehMeta(){
-		//Se algum buraco estiver superlotado, não é meta!
+	public boolean ehMeta() {
+		// Se algum buraco estiver superlotado, não é meta!
 		for (int i = 0; i < holeCapacity.size(); i++) {
 			if (holeCapacity.get(i) < 0) {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 
@@ -86,94 +101,121 @@ public class MiceAndHoles implements Estado, Heuristica, Aleatorio{
 	}
 
 	/** Lista de sucessores */
-	public List<Estado> sucessores(){
+	public List<Estado> sucessores() {
 
-		//Para o rato selecionado, crie sucessores para as possiveis posicoes que ele pode ocupar
-		if (ithMice < micePosition.size()-1) {
+		// Para o rato selecionado, crie sucessores para as possiveis posicoes
+		// que ele pode ocupar
+		if (ithMice < micePosition.size() - 1) {
 			ithMice++;
-			System.out.print("\nSucessores para mice("+ithMice+") de [");
-			for (int i = 0;i < micePosition.size(); i++) System.out.print(micePosition.get(i)+" ");
-			System.out.print("]\n");
-		}
-		else{
-			//Esta situação é estranha, precisa ser verificado se faz sentido mesmo zerar ithMice;
-			ithMice = 0;
-			System.out.print("\nSucessores para mice("+ithMice+") de [");
-			for (int i = 0;i < micePosition.size(); i++) System.out.print(micePosition.get(i)+" ");
-			System.out.print("]\n");
-		}
-
-		List<Estado> suc = new LinkedList<Estado>(); //Lista de sucessores
-
-		for (int i = 0; i < holeCapacity.size();i++){
-			MiceAndHoles novo = new MiceAndHoles(this);
-			
-			//Desaloca buracos da copia
-			for (int j = 0; j < micePosition.size(); j++){
-				novo.holeCapacity.set(novo.micePosition.get(j), novo.holeCapacity.get(novo.micePosition.get(j))+1);
+			if (debug) {
+				System.out
+						.print("\nSucessores para mice(" + ithMice + ") de [");
+				for (int i = 0; i < micePosition.size(); i++)
+					System.out.print(micePosition.get(i) + " ");
+				System.out.print("]\n");
 			}
-			
-			//Movimenta rato da copia
+		} else {
+			// Esta situação é estranha, precisa ser verificado se faz sentido
+			// mesmo zerar ithMice;
+			ithMice = 0;
+			if (debug) {
+				System.out
+						.print("\nSucessores para mice(" + ithMice + ") de [");
+				for (int i = 0; i < micePosition.size(); i++)
+					System.out.print(micePosition.get(i) + " ");
+				System.out.print("]\n");
+			}
+		}
+
+		List<Estado> suc = new LinkedList<Estado>(); // Lista de sucessores
+
+		for (int i = 0; i < holeCapacity.size(); i++) {
+			MiceAndHoles novo = new MiceAndHoles(this);
+
+			// Desaloca buracos da copia
+			for (int j = 0; j < micePosition.size(); j++) {
+				novo.holeCapacity.set(novo.micePosition.get(j),
+						novo.holeCapacity.get(novo.micePosition.get(j)) + 1);
+			}
+
+			// Movimenta rato da copia
 			novo.ithMice = ithMice;
-			novo.micePosition.set(ithMice,i);
-			//novo.holeCapacity.set(i, novo.holeCapacity.get(i)-1);
-			if (!novo.poda()){
-				//Realoca buracos
-				for (int j = 0; j < micePosition.size(); j++){
-					novo.holeCapacity.set(novo.micePosition.get(j), novo.holeCapacity.get(novo.micePosition.get(j))-1);
+			novo.micePosition.set(ithMice, i);
+
+			// Realoca buracos até o rato que está sendo processado
+			for (int j = 0; j <= ithMice; j++) {
+				novo.holeCapacity.set(novo.micePosition.get(j),
+						novo.holeCapacity.get(novo.micePosition.get(j)) - 1);
+			}
+
+			if (!novo.poda()) {
+				// Desaloca buracos da copia
+				for (int j = 0; j <= ithMice; j++) {
+					novo.holeCapacity
+							.set(novo.micePosition.get(j), novo.holeCapacity
+									.get(novo.micePosition.get(j)) + 1);
+				}
+
+				// Realoca buracos
+				for (int j = 0; j < micePosition.size(); j++) {
+					novo.holeCapacity
+							.set(novo.micePosition.get(j), novo.holeCapacity
+									.get(novo.micePosition.get(j)) - 1);
 				}
 				suc.add(novo);
 			}
 		}
 
-		//Retornar a lista de Sucessores
+		// Retornar a lista de Sucessores
 		return suc;
 	}
 
 	/**
-	 *  cria um estado inicial a partir de outro (copia)
+	 * cria um estado inicial a partir de outro (copia)
 	 */
 	MiceAndHoles(MiceAndHoles modelo) {
-		for (int i = 0; i < modelo.micePosition.size(); i++){
+		for (int i = 0; i < modelo.micePosition.size(); i++) {
 			micePosition.add(modelo.micePosition.get(i));
 			origPosition.add(modelo.origPosition.get(i));
 		}
 		for (int i = 0; i < modelo.holeCapacity.size(); i++)
 			holeCapacity.add(modelo.holeCapacity.get(i));
 	}
-	
+
 	public String toString() {
 		StringBuffer r = new StringBuffer("\n");
-		
-		//Forma de impressão do relatório 1
+
+		// Forma de impressão do relatório 1
 		r.append("\tMice position: ");
-		for (int j=0; j < micePosition.size();j++) 
-			r.append(micePosition.get(j)+" ");
+		for (int j = 0; j < micePosition.size(); j++)
+			r.append(micePosition.get(j) + " ");
 		r.append("Hole Capacity: ");
-		for (int j=0; j < holeCapacity.size();j++) 
-			r.append(holeCapacity.get(j)+" ");
+		for (int j = 0; j < holeCapacity.size(); j++)
+			r.append(holeCapacity.get(j) + " ");
 		r.append("\n");
 
-		//Forma de impressão do relatório 2
-		for (int j=0; j < holeCapacity.size();j++) {
-			r.append("\tHole("+j+"), capacity: "+holeCapacity.get(j)+" ");
-			for (int i=0; i < micePosition.size();i++) 
-				if (micePosition.get(i) == j) r.append("Mice("+i+") ");
+		// Forma de impressão do relatório 2
+		for (int j = 0; j < holeCapacity.size(); j++) {
+			r.append("\tHole(" + j + "), capacity: " + holeCapacity.get(j)
+					+ " ");
+			for (int i = 0; i < micePosition.size(); i++)
+				if (micePosition.get(i) == j)
+					r.append("Mice(" + i + ") ");
 			r.append("\n");
 		}
-		
-		r.append("Custo total: "+h()+"\n");
+
+		r.append("Custo total: " + h() + "\n");
 		System.out.print(r);
 		return r.toString();
 	}
 
-
 	public static void main(String[] a) throws IOException {
-		int micePosition[] = {4,0,6,7}; //Originalmente: 5 1 7 8 transformado em indices
-		int holeCapacity[] = {0,1,6,7,0};
+		int micePosition[] = { 4, 0, 6, 7 }; // Originalmente: 5 1 7 8
+												// transformado em indices
+		int holeCapacity[] = { 0, 1, 6, 7, 0 };
 
 		System.out.print("Iniciando... \n");
-		MiceAndHoles inicial = new MiceAndHoles(micePosition,holeCapacity);
+		MiceAndHoles inicial = new MiceAndHoles(micePosition, holeCapacity);
 
 		String str;
 		BufferedReader teclado;
@@ -182,23 +224,24 @@ public class MiceAndHoles implements Estado, Heuristica, Aleatorio{
 		Nodo n = null;
 
 		str = "0";
-		while (!str.equals("S")){
-			System.out.print("Digite sua opcao de busca { Digite S para finalizar }\n");
+		while (!str.equals("S")) {
+			System.out
+					.print("Digite sua opcao de busca { Digite S para finalizar }\n");
 			System.out.print("\t1  -  Largura\n");
 			System.out.print("\t2  -  Profundidade\n");
 			System.out.print("\t3  -  Pronfundidade Iterativo\n");
 			System.out.print("Opcao: ");
 			str = teclado.readLine().toUpperCase();
 
-			if (str.equals("1")){
+			if (str.equals("1")) {
 				System.out.println("Busca em Largura");
 				n = new BuscaLargura().busca(inicial);
-			}else{
-				if (str.equals("2")){
+			} else {
+				if (str.equals("2")) {
 					System.out.println("Busca em Profundidade");
 					n = new BuscaProfundidade(20).busca(inicial);
-				}else{
-					if (str.equals("3")){
+				} else {
+					if (str.equals("3")) {
 						System.out.println("Busca em Profundidade Iterativo");
 						n = new BuscaIterativo().busca(inicial);
 					}
@@ -206,34 +249,35 @@ public class MiceAndHoles implements Estado, Heuristica, Aleatorio{
 			}
 
 			if (n == null) {
-                System.out.println("Sem Solucao!");
-            } else {
-                System.out.println("Solucao:\n" + n.montaCaminho() + "\n\n");
-            }
+				System.out.println("Sem Solucao!");
+			} else {
+				System.out.println("Solucao:\n" + n.montaCaminho() + "\n\n");
+			}
 		}
 	}
 
-	/** Verifica se um estado eh igual a outro ja inserido na lista de sucessores (usado para poda) */
+	/**
+	 * Verifica se um estado eh igual a outro ja inserido na lista de sucessores
+	 * (usado para poda)
+	 */
 	public boolean equals(Object o) {
 		try {
 			if (o instanceof MiceAndHoles) {
-				MiceAndHoles e = (MiceAndHoles)o;
-				for (int i=0;i<micePosition.size();i++) if (this.micePosition.get(i) != e.micePosition.get(i)) return false;
-				//for (int i=0;i<holeCapacity.size();i++) if (this.holeCapacity.get(i) != e.holeCapacity.get(i)) return false;
-				//System.out.print("Equals!\n");
+				MiceAndHoles e = (MiceAndHoles) o;
+				for (int i = 0; i < micePosition.size(); i++)
+					if (this.micePosition.get(i) != e.micePosition.get(i))
+						return false;
 				return true;
 			}
 			return false;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
 
-	/** 
-	 * retorna o hashCode desse estado
-	 * (usado para poda, conjunto de fechados)
+	/**
+	 * retorna o hashCode desse estado (usado para poda, conjunto de fechados)
 	 */
 
 	public int hashCode() {
@@ -242,79 +286,57 @@ public class MiceAndHoles implements Estado, Heuristica, Aleatorio{
 
 	public int h() {
 		int movimentacao = 0;
-		for (int i = 0; i < micePosition.size(); i++){
+		for (int i = 0; i < micePosition.size(); i++) {
 			if (micePosition.get(i) != origPosition.get(i))
-				movimentacao += Math.abs(micePosition.get(i) - origPosition.get(i)); 
+				movimentacao += Math.abs(micePosition.get(i)
+						- origPosition.get(i));
 		}
-	
-		//System.out.print("h: "+movimentacao);
+
 		return movimentacao;
 	}
 
-    /**
-     * retorna true se o estado deve ser podado
-     */
-    protected boolean poda() {
-    	/*
-    	//Copia capacidades para uma lista temporária para testar inclusive os antecessores
-		//System.out.print("holeTest: ");
-    	List<Integer> holeTest = new ArrayList<Integer>();
-    	for (int i = 0; i < holeCapacity.size(); i++){
-    		holeTest.add(holeCapacity.get(i)); 
-			//System.out.print(holeTest.get(i)+" ");
-    	}
-		//System.out.print("\n");
-
-    	//Se o buraco que este rato esta tentando usar ou de seus antecessores estiver superlotado, pode podar!
-		//System.out.print("Mices("+ithMice+"): ");
-    	for (int i = 0; i <= ithMice; i++) {
-    		//System.out.print(micePosition.get(i)+"  ");
-
-    		//System.out.print("HOLETest: ");
-        	//for (int j = 0; j < holeCapacity.size(); j++){
-    			//System.out.print(holeTest.get(j)+" ");
-        	//}
-    		//System.out.print("\n");
-    		
-    		if (holeTest.get(micePosition.get(i)) < 0) {
-    			//System.out.print("\nPodar!\n");
-    			return true;
-    		}
-    		//System.out.print("\n");
-    	}
-   		
-    	return false;
-    	*/
-
-    	//Se algum buraco estiver superlotado, pode podar!
-   		if (holeCapacity.get(micePosition.get(ithMice))-1 < 0) 
-   			return true;
-   		else
-   			return false;
-   		 
-    }
+	/**
+	 * retorna true se o estado deve ser podado
+	 */
+	protected boolean poda() {
+		// Se o buraco que este rato esta tentando usar ou de seus antecessores
+		// estiver superlotado, pode podar!
+		for (int i = 0; i <= ithMice; i++) {
+			if (holeCapacity.get(micePosition.get(i)) < 0) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public Estado geraAleatorio() {
 		MiceAndHoles aleatorio = new MiceAndHoles(this);
 
-		//Desaloca buracos da copia
-		for (int j = 0; j < aleatorio.micePosition.size(); j++){
-			aleatorio.holeCapacity.set(micePosition.get(j), aleatorio.holeCapacity.get(aleatorio.micePosition.get(j))+1);
+		// Desaloca buracos da copia
+		for (int j = 0; j < aleatorio.micePosition.size(); j++) {
+			aleatorio.holeCapacity
+					.set(micePosition.get(j), aleatorio.holeCapacity
+							.get(aleatorio.micePosition.get(j)) + 1);
 		}
 
-		//Reposiciona ratos aleatoriamente
-		for (int i = 0;i < aleatorio.micePosition.size();i++)
-			aleatorio.micePosition.set(i,Math.round( (float)(Math.random()*(aleatorio.holeCapacity.size()-1))));
+		// Reposiciona ratos aleatoriamente
+		for (int i = 0; i < aleatorio.micePosition.size(); i++)
+			aleatorio.micePosition.set(i,
+					Math.round((float) (Math.random() * (aleatorio.holeCapacity
+							.size() - 1))));
 
-		//DEBUG: Novo posicionamento dos ratos
-		System.out.print("\nNovo posicionamento aleatório: ");
-		for (int i = 0;i < aleatorio.micePosition.size();i++)
-			System.out.print(aleatorio.micePosition.get(i)+" ");
-		System.out.print("\n");
-		
-		//Realoca buracos
-		for (int j = 0; j < aleatorio.micePosition.size(); j++){
-			aleatorio.holeCapacity.set(aleatorio.micePosition.get(j), aleatorio.holeCapacity.get(aleatorio.micePosition.get(j))-1);
+		if (debug) {
+			System.out.print("\nNovo posicionamento aleatório: ");
+			for (int i = 0; i < aleatorio.micePosition.size(); i++)
+				System.out.print(aleatorio.micePosition.get(i) + " ");
+			System.out.print("\n");
+		}
+
+		// Realoca buracos
+		for (int j = 0; j < aleatorio.micePosition.size(); j++) {
+			aleatorio.holeCapacity
+					.set(aleatorio.micePosition.get(j), aleatorio.holeCapacity
+							.get(aleatorio.micePosition.get(j)) - 1);
 		}
 		return aleatorio;
 	}
