cf358129e8afa4d6c084a20bfcdabc48319d129b
Cleber J. Amaral <cleberjamaral@gmail.com>
2022-05-12 14:15:49 -0300

    Installing tk support

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM gitpod/workspace-full-vnc
 RUN sudo apt-get update && \
-    sudo apt-get install -y libgtk-3-dev && \
+    sudo apt-get install -y libgtk-3-dev python-tk python3-tk tk-dev && \
     sudo rm -rf /var/lib/apt/lists/*
--- minesweeper/main.py
+++ minesweeper/main.py
@@ -1,30 +1,29 @@
 from tkinter import *
-
-WINDOW_WIDTH = 860
-WINDOW_HEIGHT = 640
+import settings
 
 root = Tk()
 # Override default setting for the window
 root.configure(bg='black')
-root.geometry(str(WINDOW_WIDTH)+'x'+str(WINDOW_HEIGHT))
+#root.geometry(f'{settings.WINDOW_WIDTH}x{settings.WINDOW_HEIGHT}')
+root.geometry(str(settings.WINDOW_WIDTH)+'x'+str(settings.WINDOW_HEIGHT))
 root.title('Minesweeper Game')
 root.resizable(False, False)
 
 top_frame = Frame(
     root,
     bg='red',
-    width=WINDOW_WIDTH,
-    height=WINDOW_HEIGHT/5
+    width=settings.WINDOW_WIDTH,
+    height=settings.WINDOW_HEIGHT/5
 )
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
     bg='blue',
-    width=WINDOW_WIDTH/5,
-    height=WINDOW_HEIGHT-WINDOW_HEIGHT/5
+    width=settings.WINDOW_WIDTH/5,
+    height=settings.WINDOW_HEIGHT-settings.WINDOW_HEIGHT/5
 )
-left_frame.place(x=0, y=WINDOW_HEIGHT/5)
+left_frame.place(x=0, y=settings.WINDOW_HEIGHT/5)
 
 # Run the window
 root.mainloop()
