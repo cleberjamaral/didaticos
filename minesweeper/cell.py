b8c2a5edb503c699ef0c4a1207744d752d643b4c
Cleber Jorge Amaral <cleberjamaral@gmail.com>
2022-05-15 13:12:14 +0000

    Jogo minesweeper funcionando

--- minesweeper/cell.py
+++ minesweeper/cell.py
@@ -1,12 +1,19 @@
 from tkinter import Button, Label
+from tkinter import messagebox
+import tkinter.font as font
 import random
 import settings
+import sys
 
 class Cell:
     all = []
+    cell_count = settings.CELLS_COUNT
     cell_count_label_object = None
+    btn_face_color = None
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
+        self.is_oneped = False
+        self.is_mine_candidate = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
@@ -17,11 +24,13 @@ class Cell:
     def create_btn_object(self, location):
         btn = Button(
             location,
-            width=12,
-            height=4
+            font=font.Font(weight="bold", size=16),
+            width=6,
+            height=2,
         )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
+        self.btn_face_color = btn.cget("background")
         self.cell_btn_object = btn
 
     @staticmethod
@@ -30,7 +39,7 @@ class Cell:
             location,
             bg='black',
             fg='white',
-            text=f"Cells Left: {settings.CELLS_COUNT}",
+            text=f"Cells Left: {Cell.cell_count}",
             font=("", 30)
         )
         Cell.cell_count_label_object = lbl
@@ -43,6 +52,12 @@ class Cell:
                 for cell in self.surrounded_cells:
                     cell.show_cell()
             self.show_cell()
+            if Cell.cell_count == settings.MINES_COUNT:
+                messagebox.showinfo(
+                    "You won!",
+                    "Congratulations, you've found all mines!"
+                )
+
 
     def get_cell_by_axis(self, x, y):
         for cell in Cell.all:
@@ -73,15 +88,36 @@ class Cell:
         return counter
 
     def show_cell(self):
-        self.cell_btn_object.configure(
-            text=f'{self.surrounded_cells_mines_length}'
-        )
+        if not self.is_oneped:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(
+                text=f'{self.surrounded_cells_mines_length}',
+                bg=self.btn_face_color
+            )
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                text=f"Cells Left: {Cell.cell_count}"
+            )
+        self.is_oneped = True
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
+
 
     def show_mine(self):
         self.cell_btn_object.configure(bg="red")
+        messagebox.showinfo(
+            "Game over!",
+            "You click on a mine!"
+        )
+        sys.exit()
 
     def right_click_actions(self, event):
-        print(event)
+        if not self.is_mine_candidate:
+            self.cell_btn_object.configure(bg="orange")
+            self.is_mine_candidate = True
+        else:
+            self.cell_btn_object.configure(bg=self.btn_face_color)
+            self.is_mine_candidate = False
 
     @staticmethod
     def randomize_mines():
--- minesweeper/main.py
+++ minesweeper/main.py
@@ -19,6 +19,17 @@ top_frame = Frame(
 )
 top_frame.place(x=0, y=0)
 
+game_title = Label(
+    top_frame,
+    bg='black',
+    fg='white',
+    text='Minesweeper Game',
+    font=(', 48')
+)
+game_title.place(
+    x=utils.width_prct(25), y=0
+)
+
 left_frame = Frame(
     root,
     bg='black',
