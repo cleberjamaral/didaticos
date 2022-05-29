3280a2b0a42cc590d60122bfc69629248a1c15b2
cleberjamaral <cleberjamaral@gmail.com>
2022-05-28 21:57:06 -0300

    Detectando colisão

--- selfdrivingcar/main.js
+++ selfdrivingcar/main.js
@@ -1,16 +1,16 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 200;
+const canvas=document.getElementById("myCanvas");
+canvas.width=200;
 
-const ctx=canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9);
 const car=new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
 
-function animate() {
+function animate(){
     car.update(road.borders);
 
-    canvas.height = window.innerHeight;
+    canvas.height=window.innerHeight;
 
     ctx.save();
     ctx.translate(0,-car.y+canvas.height*0.7);
@@ -21,4 +21,3 @@ function animate() {
     ctx.restore();
     requestAnimationFrame(animate);
 }
-
